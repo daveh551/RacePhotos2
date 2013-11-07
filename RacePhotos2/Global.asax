81ae5e8f8a738a3b588c3810fbd54179dc36d84e
Dave Hanna <dave@infotouchsys.com>
2013-11-07 15:26:40 -0600

    Initial project creation from MVC5 Template

---
+++ RacePhotos2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RacePhotos2.MvcApplication" Language="C#" %>
---
+++ RacePhotos2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RacePhotos2.Startup))]
+namespace RacePhotos2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
